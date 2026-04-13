b6f9bee37b49df5e091c7bfd6cc2468e905f66d1
Brian Sheldon <brian.sheldon@live.ca>
2026-04-13 14:14:38 -0700

    0.123 mhz to 1.170 mhz

--- rom-emu-test/rom-emu-test.ino
+++ rom-emu-test/rom-emu-test.ino
@@ -67,6 +67,10 @@ void initRom() {
   }
 }
 
+int addrPinMask, wrPinMask, rdPinMask, mreqPinMask;
+int dataPinMask, clkPinMask;
+int specPinMask, ledPinMask;
+
 void setup() {
   Serial.begin( 115200 );
   delay( 200 );
@@ -75,11 +79,37 @@ void setup() {
 
   pinMode( LED_BUILTIN, OUTPUT );
   pinMode( 26, INPUT_PULLUP );
+  
+  
+  // pin masks    3322222222221111111111
+  //              10987654321098765432109876543210
+  addrPinMask = 0b00000000000000000000011111111111;  // gp0-10
+  wrPinMask   = 0b00000000000000000000100000000000;  // gp11
+  rdPinMask   = 0b00000000000000000001000000000000;  // gp12
+  mreqPinMask = 0b00000000000000000010000000000000;  // gp13
+  dataPinMask = 0b00000000001111111100000000000000;  // gp14-21
+  clkPinMask  = 0b00000000010000000000000000000000;  // gp22
+  specPinMask = 0b00000000100000000000000000000000;  // gp23 - must not be changed
+  ledPinMask  = 0b00000010000000000000000000000000;  // gp25 - builtin led
+  
+  gpio_init_mask( addrPinMask );
+  gpio_init_mask( wrPinMask );
+  gpio_init_mask( rdPinMask );
+  gpio_init_mask( mreqPinMask );
+  gpio_init_mask( dataPinMask );
+  gpio_init_mask( clkPinMask );
+  
+  gpio_set_dir_in_masked( addrPinMask );
+  gpio_set_dir_in_masked( wrPinMask );
+  gpio_set_dir_in_masked( rdPinMask );
+  gpio_set_dir_in_masked( mreqPinMask );
+  gpio_set_dir_in_masked( dataPinMask );  // initially start as inputs, but change to outputs during writes
+  gpio_set_dir_out_masked( clkPinMask );
 
-  initPins( addrPins, 11, INPUT );
-  initPins( ctrlPins, 3, INPUT );
-  initPins( dataPins, 8, INPUT );
-  pinMode( clockPin, OUTPUT );
+  //initPins( addrPins, 11, INPUT );
+  //initPins( ctrlPins, 3, INPUT );
+  //initPins( dataPins, 8, INPUT );
+  //pinMode( clockPin, OUTPUT );
 }
 
 int clk = 0;
@@ -100,21 +130,27 @@ bool first_req = true;
 //
 void loop() {
   // Clock is high
-  if ( digitalRead( mreqPin ) == 0 ) {
-    addr = getAddr();
-    if ( digitalRead( rdPin ) == 0 ) {
+  if ( ( gpio_get_all() & mreqPinMask ) == 0 ) {  // up to 1.163 mhz
+  //if ( digitalRead( mreqPin ) == 0 ) {
+    addr = gpio_get_all() & addrPinMask;  // using this instead of getAddr increased speed from 0.279 mhz to 0.439 mhz
+    //addr = getAddr();
+    if ( ( gpio_get_all() & rdPinMask ) == 0 ) {  // up to 0.993 mhz
+    //if ( digitalRead( rdPin ) == 0 ) {
       if ( first_req ) {
         //
         // Read request
         //
         first_req = false;
         data = rom[addr];
-        initPins( dataPins, 8, OUTPUT );
-        putData( data );
+        gpio_set_dir_out_masked( dataPinMask );  // using this instead of initPins in both places increased speed from 0.123 mhz to 0.208 mhz
+        //initPins( dataPins, 8, OUTPUT );
+        gpio_put_masked( dataPinMask, data << 14 );  // using this instead of putData increased speed from 0.208 mhz to 0.279 mhz
+        //putData( data );
         //sprintf( buffer, "Reading: %04x %02x", addr, data );
         //Serial.println( buffer );
       }
-    } else if ( digitalRead( wrPin ) == 0 ) {
+    } else if ( ( gpio_get_all() & wrPinMask ) == 0 ) {  // up to 0.993 mhz
+    //} else if ( digitalRead( wrPin ) == 0 ) {
       if ( first_req ) {
         //
         // Write request
@@ -122,46 +158,50 @@ void loop() {
         first_req = false;
         data = getData();
         rom[addr] = data;
-        if ( ( writecount % 2000 ) == 0 ) {
+        if ( ( writecount++ % 16000 ) == 0 ) {
           sprintf( buffer, "Mhz: %02.3f Writing: %04x %02x", mhz, addr, data );
           Serial.println( buffer );
         }
-        writecount++;
       }
     }
   } else {
     if ( ! first_req ) {
       first_req = true;
-      initPins( dataPins, 8, INPUT );
+      gpio_set_dir_in_masked( dataPinMask );  // using this instead of initPins in both places increased speed from 0.123 mhz to 0.208 mhz
+      //initPins( dataPins, 8, INPUT );
     }
   }
   // set clock low
-  digitalWrite( clockPin, 0 );
+  gpio_put_masked( clkPinMask, 0 );  // replacing digitalWrite in this and the below case increased speed from 0.439 mhz to 0.798 mhz
+  //digitalWrite( clockPin, 0 );
   //digitalWrite( LED_BUILTIN, LOW );
   //if ( delayTime > 0 ) {
-    delay( delayTime );
+    //delay( delayTime );  // commented out both delays increased speed from 0.798 mhz to 0.872 mhz
   //}
   // set clock high
-  if ( ( count % 100 ) == 0 ) {
-    if ( ( count % 10000 ) == 0 ) {
+  //if ( ( count % 100 ) == 0 ) {
+    if ( ( count % 1000000 ) == 0 ) {  // change count from 100,000 1,000,000 1.171 mhz
       unsigned long currTime = micros();
       unsigned long diffTime = currTime - lastTime;
       //Serial.println( currTime );
       if ( diffTime > 0.0 ) {
-        mhz = 10000.0 / diffTime;
+        mhz = 1000000.0 / diffTime;
       }
       lastTime = currTime;
     }
+    /*
     if ( digitalRead( 26 ) == 0 ) {
       delayTime = 0;
     } else {
       delayTime = 0;
     }
-  }
-  digitalWrite( clockPin, 1 );
+    */
+  //}
+  gpio_put_masked( clkPinMask, clkPinMask );  // replacing digitalWrite in this and the above case increased speed from 0.439 mhz to 0.798 mhz
+  //digitalWrite( clockPin, 1 );
   //digitalWrite( LED_BUILTIN, HIGH );
   //if ( delayTime > 0 ) {
-    delay( delayTime );
+    //delay( delayTime );  // commented out both delays increased speed from 0.798 mhz to 0.872 mhz
   //}
   count++;
 }
